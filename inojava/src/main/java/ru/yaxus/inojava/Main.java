b1748b09a8de8f66bbf1f9c3b4e014ed861438a7
Ilya Yakushev <flomaster44@gmail.com>
2025-07-20 18:32:52 +0300

    HW 03 2

--- inojava/src/main/java/ru/yaxus/inojava/Main.java
+++ inojava/src/main/java/ru/yaxus/inojava/Main.java
@@ -3,7 +3,7 @@ package ru.yaxus.inojava;
 public class Main {
 
     public static void main(String[] args) {
-        new ru.yaxus.inojava.homeworks.HW03_1();
+        new ru.yaxus.inojava.homeworks.HW03_2();
     }
 
 }
