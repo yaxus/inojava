dfb9a950ae132632ddcdf7347451775486224144
Ilya Yakushev <flomaster44@gmail.com>
2025-08-17 18:40:41 +0300

    Изменена проверка инициализации продукта в соответствии с новыми условиями задачи

--- src/HW07/Product/BaseProduct.java
+++ src/HW07/Product/BaseProduct.java
@@ -1,6 +1,8 @@
 package HW07.Product;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BaseProduct implements ProductInterface {
 
@@ -13,8 +15,12 @@ public class BaseProduct implements ProductInterface {
     }
 
     protected void _setName(String name) throws IllegalArgumentException {
-        if (name.isEmpty()){
-            throw new IllegalArgumentException("Название продукта не может быть пустой строкой");
+        Pattern onlyDigitsPattern = Pattern.compile("^\\d+$");
+        Matcher matcher = onlyDigitsPattern.matcher(name);
+        if (name.length() < 3){
+            throw new IllegalArgumentException("Название продукта не может быть короче 3 символов, попытка установить: " + name.length());
+        } else if (matcher.matches()) {
+            throw new IllegalArgumentException("Название продукта не может состоять из одних цифр, попытка установить: " + name);
         }
         this.name = name;
     }
@@ -24,8 +30,8 @@ public class BaseProduct implements ProductInterface {
     }
 
     protected void _setPrice(double price) throws IllegalArgumentException {
-        if (price < 0){
-            throw new IllegalArgumentException("Стоимость продукта не может быть отрицательной, попытка установить: " + price);
+        if (price <= 0){
+            throw new IllegalArgumentException("Стоимость продукта не может быть отрицательной или равна 0, попытка установить: " + price);
         }
         this.price = price;
     }
